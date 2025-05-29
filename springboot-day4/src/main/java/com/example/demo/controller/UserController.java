59a054b4a4f2fd9d82ff0b6ca71f904e0c2e6a0e
Ragavi <49680219+RagaviVenkatesh@users.noreply.github.com>
2025-05-29 11:09:43 +0530

    Add files via upload

---
+++ springboot-day4/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.UserDTO;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<UserDTO> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping
+    public UserDTO createUser(@RequestBody UserDTO userDTO) {
+        return userService.createUser(userDTO);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<UserDTO> getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+}
---
+++ springboot-day4/src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,38 @@
+package com.example.demo.service;
+
+import com.example.demo.dto.UserDTO;
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public List<UserDTO> getAllUsers() {
+        return userRepository.findAll().stream()
+            .map(user -> modelMapper.map(user, UserDTO.class))
+            .collect(Collectors.toList());
+    }
+
+    public UserDTO createUser(UserDTO userDTO) {
+        User user = modelMapper.map(userDTO, User.class);
+        return modelMapper.map(userRepository.save(user), UserDTO.class);
+    }
+
+    public Optional<UserDTO> getUserById(Long id) {
+        return userRepository.findById(id)
+            .map(user -> modelMapper.map(user, UserDTO.class));
+    }
+}
